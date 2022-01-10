5b689eb3511c78e6d29caccffcefb42861d026a0
hasnashab <78135389+hasnashab@users.noreply.github.com>
2022-01-10 23:10:44 +0700

    Add project files.

---
+++ WcfService_2Way_056/IClientCallback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService_2Way_056
+{
+    [ServiceContract]
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesanKirim(string user, string pesan);
+    }
+}
